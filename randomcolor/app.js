b81ddf742763759cc9c6e27f7b6b5f105d380949
ANURAG0S1 <anuragkush051@gmail.com>
2020-10-12 08:55:20 +0530

    randomcolor ui updated

--- randomcolor/app.js
+++ randomcolor/app.js
@@ -1,18 +1,29 @@
-// setInterval( 
-    function randome(){
-    var x = Math.floor((Math.random())*256);
-    var y = Math.floor((Math.random())*256);
-    var z = Math.floor((Math.random())*256);
-    var Bicolor = "rgb("+x+","+ y +"," + z+")";
+function randome() {
+  var node = document.createElement("LI");
+  node.className = "coll";
+  var x = Math.floor(Math.random() * 256);
+  var y = Math.floor(Math.random() * 256);
+  var z = Math.floor(Math.random() * 256);
+  Bicolor = "rgb(" + x + "," + y + "," + z + ")";
+  document.body.style.backgroundColor = Bicolor;
 
-document.body.style.backgroundColor = Bicolor;
+  document.querySelector("#code").innerHTML = Bicolor;
+  node.textContent = Bicolor;
+  node.style.border = "2px solid white";
+  node.style.backgroundColor = node.textContent;
+  node.addEventListener("click", function () {
+    document.body.style.backgroundColor = node.textContent;
+    // document.querySelector("#ite").style.backgroundColor = node.textContent;
+    document.querySelector("#code").innerHTML = node.textContent;
+  });
 
-  console.log(Bicolor);
- 
-
- 
-
- 
+  document.querySelector("#ite").appendChild(node);
 }
-// , 1000 );
- 
+function ho() {
+  document.querySelector("#h-list").style.display = "block";
+  document.querySelector("#history").setAttribute("onclick", "hc()");
+}
+function hc() {
+  document.querySelector("#history").setAttribute("onclick", "ho()");
+  document.querySelector("#h-list").style.display = "none";
+}
