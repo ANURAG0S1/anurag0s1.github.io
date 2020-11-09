9aaf901cf884768a43f9a41c869aa4dc308da0ce
ANURAG0S1 <anuragkush051@gmail.com>
2020-11-09 21:32:00 +0530

    added folder portfolio revised

---
+++ portfolio-revised/script.js
@@ -0,0 +1,6 @@
+function Showmes() {
+  document.querySelector("#mes-box").style.transform = "scale(1)";
+}
+function Closemes() {
+  document.querySelector("#mes-box").style.transform = "scale(0)";
+}
