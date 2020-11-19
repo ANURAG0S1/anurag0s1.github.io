cf193a32340cb45a21c599f62265c43fc1a25cea
ANURAG0S1 <anuragkush051@gmail.com>
2020-11-19 19:08:27 +0530

    .

--- assets/navbar/js/script.js
+++ assets/navbar/js/script.js
@@ -1,14 +1,8 @@
 var btn = document.querySelector('#item-1')
 var btn2 = document.querySelector('#btn2')
 var targett = document.querySelector("#search")
-<<<<<<< Updated upstream
-var logo = document.querySelector("#logo")
-var more_menu_btn = document.querySelector('#menu-4')
-var more_menu = document.querySelector('#more-menu')
-=======
 var more_menu_btn = document.querySelector('#more_menu_btn')
 var more_menuu = document.querySelector('#more-menu')
->>>>>>> Stashed changes
 var pro_menu = document.querySelector('#profile_menu')
 var pro_menu_btn = document.querySelector('#item-3')
 
@@ -16,10 +10,8 @@ function Show_search() {
     targett.style.transform = 'scale(1)';
     btn2.setAttribute('onclick', 'Hide_search()')
     btn.style.display = 'none'
-    targett.style.display = 'block'
-    if (window.innerWidth < 600) {
-        logo.style.display = 'none';
-    }
+    targett.style.display = 'flex'
+
 }
 
 function Hide_search() {
@@ -28,12 +20,6 @@ function Hide_search() {
     targett.style.display = 'none'
 
     btn.setAttribute('onclick', 'Show_search()')
-<<<<<<< Updated upstream
-    if (window.innerWidth < 600) {
-        logo.style.display = 'block';
-    }
-}
-=======
 
 }
 
@@ -45,4 +31,3 @@ function more_menu() {
     }
 }
 document.querySelector('#bottom-bar li').addEventListener(onclick, more_menu())
->>>>>>> Stashed changes
